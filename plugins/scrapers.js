66a1433bd7d3d0309f6292d402b8d280dc063834
br-ut <42611524+br-ut@users.noreply.github.com>
2021-03-25 15:35:06 +0000

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -33,9 +33,6 @@ const spotifyApi = new SpotifyWebApi({
     clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'
 });
 //=====================================================================================
-//=================================== INSTAGRAM =======================================
-const instagram_download = require ('@juliendu11/instagram-downloader');
-//=====================================================================================
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
 const Glang = Language.getString('github');
@@ -189,30 +186,6 @@ if (config.WORKTYPE == 'private') {
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
             await message.client.sendMessage(message.jid,fs.readFileSync('./' + arama.videoId + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4});
-        });
-    }));
-
-    Asena.addCommand({pattern: 'ig ?(.*)', fromMe: true, desc: Lang.IG_VIDEO_DESC}, (async (message, match) => {
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.IG_NEED_VIDEO,MessageType.text);
-
-        try {
-                    var arama = match[1];
-                } catch {
-                    return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-                }
-
-        var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_VIDEO,MessageType.text);
-
-        async () => {
-            reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            const igvid = await instagram_download.downloadMedia(arama, './')
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + igvid), MessageType.video, {mimetype: Mimetype.mp4});
         });
     }));
 
